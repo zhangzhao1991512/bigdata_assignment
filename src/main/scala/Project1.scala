f50b883bb5342d465dba18dbe3b51820f58924db
jackzh <jackzh@node-0.jackzh-p1.michigan-bigdata-pg0.clemson.cloudlab.us>
2017-10-03 21:38:51 -0400

    udf and exploding working

--- src/main/scala/Project1.scala
+++ src/main/scala/Project1.scala
@@ -1,60 +1,74 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.explode
+import org.apache.spark.sql.DataFrame
 //import spark.implicits._
 
-//function with input string "text" that returns a list of strings
-def getLinks (textIn: String) : List[String] = textIn match {
-	case null => {List[String]()}
-	case text => {
-		val linkPattern = "\\[\\[(.*?)\\]\\]".r
-		val matchList = linkPattern.findAllIn(text).toList //create a list of all elements that fit in between [[   ]]
-
-		val blank = "\\[\\[\\s*\\]\\]".r //used to filter out blank entries
-		val first_link_empty = "\\[\\[\\s*\\|+.*\\]\\]".r //used to filter out entries that are blank before the pipe
-		val extra = "\\|.*".r //used to remove any entries after the pipe
-
-		val category = "\\[\\[Category:".r
-
-		//filter out all invalid cases
-		val matchList = for {
-			link <- matchList
-			if(blank.findFirstIn(link) == None && first_link_empty.findFirstIn(link) == None 
-						   && !link.contains("#") && (!link.contains(":") || category.findFirstIn(link) != None))
-		} yield link.replaceAll("[", "").replaceAll("]", "").replaceAll(extra, "").toLowerCase
-		
-		//remove all empty elements from the list
-		val newListNoEmpty = for {
-			link <- newList
-			if link.nonEmpty
-		} yield link
-		return newListNoEmpty
-	}
-}
-
-
 object Project1 {
-  def main(args: Array[String]) {
-    val appleFile = "hdfs://clnode163.clemson.cloudlab.us:8020/p1/Apple.xml" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Project1").getOrCreate()
+
+	//function with input string "text" that returns a list of strings
+	def getLinks (textIn: String) : List[String] = textIn match {
+		case null => {List[String]()}
+		case text => {
+			val linkPattern = "\\[\\[(.*?)\\]\\]".r
+			val linkList = linkPattern.findAllIn(text).toList //create a list of all elements that fit in between [[   ]]
+
+			val blank = "\\[\\[\\s*\\]\\]".r //used to filter out blank entries
+			val first_link_empty = "\\[\\[\\s*\\|+.*\\]\\]".r //used to filter out entries that are blank before the pipe
+			val extra = "\\|.*" //used to remove any entries after the pipe
+
+			val category = "\\[\\[Category:".r
+
+			//filter out all invalid cases
+			val linkListFiltered = for {
+				link <- linkList
+				if(blank.findFirstIn(link) == None && first_link_empty.findFirstIn(link) == None 
+						   && !link.contains("#") && (!link.contains(":") || category.findFirstIn(link) != None))
+			} yield link.replace("[", "").replace("]", "").replaceAll(extra, "").toLowerCase
+		
+			//remove all empty elements from the list
+			val linkListNoEmpty = for {
+				link <- linkListFiltered
+				if !link.isEmpty
+			} yield link
+			return linkListNoEmpty
+		}
+	}	
+
+
+	val udfGetLinks = udf[List[String], String](getLinks)
+
+	def main(args: Array[String]) {
+		val appleFile = "hdfs://clnode163.clemson.cloudlab.us:8020/p1/Apple.xml" // Should be some file on your system
+		val spark = SparkSession.builder.appName("Project1").getOrCreate()
   
-    import spark.implicits._
+		import spark.implicits._
 
-    val df0 = spark.read
-      .format("com.databricks.spark.xml")
-      .option("rowTag", "page")
-      .load(appleFile)
+		val df0 = spark.read
+      		.format("com.databricks.spark.xml")
+     		.option("rowTag", "page")
+                .option("excludeAttribute", true)
+		.load(appleFile)
+	
+		//val selectedData = df.select("author", "_id")
+	    //selectedData.write
+	      //.format("com.databricks.spark.xml")
+	      //.option("rootTag", "books")
+	      //.option("rowTag", "book")
+	      //.save("newbooks.xml") 
+		df0.printSchema()
 
-	//val selectedData = df.select("author", "_id")
-    //selectedData.write
-      //.format("com.databricks.spark.xml")
-      //.option("rootTag", "books")
-      //.option("rowTag", "book")
-      //.save("newbooks.xml") 
-	df.printSchema()
 
+		val df1 =  df0.select("title","revision.text")
+		df1.show()
 
-    df1 =  df0.select("title","revision.text")
-    df1.show()
+		val df2 = df1.withColumn("linkList", udfGetLinks('text))
+		df2.show()
+		df2.select("linkList").show()
+		df2.printSchema()	
+		val df3 = df2.withColumn("links",explode(df2("linkList")))
+		df3.show()
 
-    spark.stop()
-  }
+		spark.stop()
+	}
 }
