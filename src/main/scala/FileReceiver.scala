8531feeb21f4c5ca6054a37ff7aa3926fe35d30f
jackzh <jackzh@node-0.jackzh-p1.michigan-bigdata-pg0.clemson.cloudlab.us>
2017-10-09 22:49:27 -0400

    rough draft

--- src/main/scala/FileReceiver.scala
+++ src/main/scala/FileReceiver.scala
@@ -22,20 +22,30 @@ object FileReceiver {
         text("hdfs://clnode140.clemson.cloudlab.us:8020/p1/q10input/")
 
       
- //     val rank = spark.
- //       read.
- //       text("hdfs://clnode140.clemson.cloudlab.us:8020/p1/q10input/")
-
 	rank.printSchema()
 	
 // val largeRanks = rank.filter("rank <= 0.5")
 
     println(rank.isStreaming)
 
-     val query = rank.writeStream
+/*
+1. convert dataFrame into dataFrame of two columns (column 1 is article name, column 2 is rank) do this with udf
+2. filter out articles with rank less than (or equal to) 0.5
+3. make finalDataFrame by selecting desired columns from intermediate
+*/
+
+/*
+     val query = rank.select("value").writeStream
         .outputMode("append")
         .format("console")
         .start()
+*/
+     val query = finalDataFrame.select("articleName","rank").writeStream
+        .outputMode("append")
+        .format("csv")
+	.option("sep","\t")
+        .option("path", "path/to/destination/dir")
+	.start()
 
      query.awaitTermination()
 
